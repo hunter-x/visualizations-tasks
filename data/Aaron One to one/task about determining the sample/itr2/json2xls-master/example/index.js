4d326ef39e93d6f804f458207fd8a04913647d49
hunter-x <aa.test20@gmail.com>
2018-06-03 13:17:24 +0100

    Adding olld Aaron project data

--- data/Aaron One to one/task about determining the sample/itr2/json2xls-master/example/index.js
+++ data/Aaron One to one/task about determining the sample/itr2/json2xls-master/example/index.js
@@ -1,8 +1,8 @@
 var json2xls = require('../lib/json2xls');
-var data = require('./Final2-sample+validVotes.json');
+var data = require('./2-FINAL validVotes-blank-registration-per mun.json');
 var fs = require('fs');
 
 var xls = json2xls(data,{});
 
 
-fs.writeFileSync('Final2-sample+validVotes.xlsx',xls, 'binary');
+fs.writeFileSync('2-FINAL validVotes-blank-registration-per mun.xlsx',xls, 'binary');

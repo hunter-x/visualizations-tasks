9f2259903a2d6924fcb703c15cc912358393da94
hunter-x <aa.test20@gmail.com>
2018-06-04 12:42:54 +0100

    routes contains path to cso & tadeem maps

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import RootHome from './components/home/RootHome' ;
 
 import PPMain from './components/pp-proposal/PPMain' ;
 import _RootMunTurnout from './components/pp-proposal/turnout/_RootMunTurnout' ;
+import _RootCsoNumber from './components/pp-proposal/cso/_RootCsoNumber' ;
+import _RootTadeem from './components/pp-proposal/tadeem/_RootTadeem' ;
 
 import counterpart  from 'counterpart';
 counterpart.registerTranslations('en',require('./../locales/en'));
@@ -18,6 +20,8 @@ export default class App extends Component {
       
       <Route exact path="/pp-proposal" component={PPMain} />
       <Route exact path="/municipal-turnout18" component={_RootMunTurnout} />
+      <Route exact path="/cso-stat" component={_RootCsoNumber} />
+      <Route exact path="/tadeem" component={_RootTadeem} />
 
       </Switch>
     );

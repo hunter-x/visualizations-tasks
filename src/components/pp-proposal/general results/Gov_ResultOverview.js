f024f77b58a21d6970d13d2ee9107c34e55eff4c
hunter-x <aa.test20@gmail.com>
2018-06-11 02:20:28 +0100

    dynamic party result  bar charts

--- src/components/pp-proposal/general results/Gov_ResultOverview.js
+++ src/components/pp-proposal/general results/Gov_ResultOverview.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
 import Translate from 'react-translate-component';
-import Nav from '../../shared/Nav';
-import TadeemMap from './TadeemMap';
 import party_res from './all_parties_res.js'
 import mun_to_gov from './Governorates_associated to municipality.js'
 import _ from 'lodash';
 import ResultItem from './ResultItem';
+import { Link} from 'react-router-dom';
 
 export default class Gov_ResultOverview extends Component {
 
@@ -18,54 +17,45 @@ export default class Gov_ResultOverview extends Component {
     //get the name of the gov- start reading from position 13 after /mun-results/
     let RES = [];
     let chosenGov = (this.props.location.pathname).substring(13);
-
+    console.log(chosenGov);
     //get all the municipalities of a certain gov
     var munsOfGov = _.filter(mun_to_gov, function (o) { return o.GOV_EN == chosenGov; });
-
+    console.log(munsOfGov);
     //get results of specified municipalities from the result array
     for (let i = 0; i < munsOfGov.length; i++) {
       const MunNameAr = munsOfGov[i].NAME_AR;
       RES.push(_.filter(party_res, function (o) { return o.map_names_ar == MunNameAr; }))
     }
     //sorting final RES array
-    let arrayOfMun= [],obj= {},SORTED_ARRAY=[]
+    let arrayOfMun = [], obj = {}, SORTED_ARRAY = []
     for (let j = 0; j < RES.length; j++) {
-      obj.id=j;
-      obj.mun_fr=RES[j][1].mun_fr;
+      obj.id = j;
+      obj.mun_fr = RES[j][0].mun_fr;
       arrayOfMun.push(obj);
-      obj={}
+      obj = {}
     }
-   /*  ArrayIndex=_.sortBy(arrayOfMun, [function(o) { return o.mun; }]); */
+    /*  ArrayIndex=_.sortBy(arrayOfMun, [function(o) { return o.mun; }]); */
     arrayOfMun.sort(compare);
-    console.log(arrayOfMun);
 
     //reconstruct sorted array
     for (let k = 0; k < arrayOfMun.length; k++) {
-      SORTED_ARRAY[k]=RES[arrayOfMun[k].id]
+      SORTED_ARRAY[k] = RES[arrayOfMun[k].id]
     }
+    console.log(SORTED_ARRAY);
+
     this.setState({ data: SORTED_ARRAY, mun_name: chosenGov });
   }
 
-  componentDidMount() {
-    (function () {
-      var $body = document.body
-        , $menu_trigger = $body.getElementsByClassName('menu-trigger')[0];
-
-      if (typeof $menu_trigger !== 'undefined') {
-        $menu_trigger.addEventListener('click', function () {
-          $body.className = ($body.className == 'menu-active') ? '' : 'menu-active';
-        });
-      }
-
-    }).call(this);
-  }
 
   render() {
     const TITLE = <Translate type='text' content='mun_res_box.title' />//...Governorate result
 
     return (
-      <div id="content">
-        <div className="menu-trigger"></div>
+      <div id="contentBlog">
+        <div className="menu-trigger"><Link to="/mun-results">
+          <button className="btn success" >Back</button>
+        </Link></div>
+        
         <div className="site-content" style={{ marginBottom: '5vh' }}>
           <h1 className="site-content__headline">{(this.props.location.pathname).substring(13)}{TITLE}</h1>
         </div>
@@ -74,9 +64,10 @@ export default class Gov_ResultOverview extends Component {
             (this.state.data).map((object, i) =>
               <ResultItem
                 key={i}
+                data={this.state.data[i]}
                 mun_name={object[0].mun_fr}
-                turnout={((object[0].total_votes*100)/object[0].total_inscrits).toFixed(2)}
-                blank={((object[0].votes_blancs*100)/object[0].total_votes_valide).toFixed(2)}
+                turnout={((object[0].total_votes * 100) / object[0].total_inscrits).toFixed(2)}
+                blank={((object[0].votes_blancs * 100) / object[0].total_votes_valide).toFixed(2)}
               />
             )
           }
@@ -88,7 +79,7 @@ export default class Gov_ResultOverview extends Component {
   }
 }
 //lodash _sortBy disn't want to sort
-function compare(a,b) {
+function compare(a, b) {
   if (a.mun_fr < b.mun_fr)
     return -1;
   if (a.mun_fr > b.mun_fr)
--- src/components/pp-proposal/general results/ResultItem.js
+++ src/components/pp-proposal/general results/ResultItem.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import  {BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend,ResponsiveContainer} from 'Recharts' ;
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'Recharts';
 import Translate from 'react-translate-component';
 import './result-item.css'
 export default class ResultItem extends Component {
@@ -13,35 +13,50 @@ export default class ResultItem extends Component {
         const TURNOUT = <Translate type='text' content='mun_res_box.turnout' />//Turnout
         const BLANK = <Translate type='text' content='mun_res_box.blank' />//Blank
         const SEATS_NUMBER = <Translate type='text' content='mun_res_box.seats_num' />//seats number
-        const data = [
-            {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
-            {name: 'Page B', uv: 3000, pv: 1398, amt: 2210}
-      ];
+        console.log(this.props.data);
+        let data = [];
+        (this.props.data).map((object, i) => {
+            data.push({
+                name: object.nom_liste_fr, seats: parseInt(object.sieges_obtenus), fill: object.fill,
+                votes_obtenus: parseInt(object.votes_obtenus)
+
+            })
+        })
+        data.sort(compare);
+
         return (
             <div className="blog-item">
                 <div className="thumb1">
-                <ResponsiveContainer width='100%' height={380}>
-                <BarChart  data={data}
-                margin={{top: 5, right: 30, left: 20, bottom: 5}}>
-           <CartesianGrid strokeDasharray="3 3"/>
-           <XAxis dataKey="name"/>
-           <YAxis/>
-           <Tooltip/>
-           <Legend />
-           <Bar dataKey="pv" fill="#8884d8" />
-           <Bar dataKey="uv" fill="#82ca9d" />
-          </BarChart>
-          </ResponsiveContainer>                    
+                    <ResponsiveContainer width='100%' height={380}>
+                        <BarChart data={data}
+                            margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <XAxis dataKey="name" />
+                            <YAxis />
+                            <Tooltip />
+                            <Legend />
+                            <Bar dataKey="seats" />
+                        </BarChart>
+                    </ResponsiveContainer>
                 </div>
                 <div className="article">
                     <span className="tag">{this.props.mun_name}</span>
-                    <h4>{TURNOUT} {this.props.turnout} %</h4>
-                    <h4>{BLANK} {this.props.blank} %</h4>
-                    <h4>{SEATS_NUMBER} _</h4>
+                    <div style={{textAlign:'center'}} >
+                    <div className='text-margin-top'><h4 style={{ display: 'inline' }} >{TURNOUT}</h4>  <h4  className="subheaderTitle inline"> {this.props.turnout} %</h4></div>
+                    <div className='text-margin-top' > <h4 style={{display:'inline'}}>{BLANK}</h4><h4 className="subheaderTitle inline"> {this.props.blank} %</h4></div>
+                    <div className='text-margin-top' > <h4 style={{display:'inline'}}>{SEATS_NUMBER}</h4><h4 className="subheaderTitle inline"> _</h4></div>
+                    </div>
                     <hr />
-                    <h5 className="author">By Adam J</h5>
+                    <h5 className="author"></h5>
                 </div>
             </div>
         );
     }
+}
+function compare(a, b) {
+    if (a.seats < b.seats)
+        return -1;
+    if (a.seats > b.seats)
+        return 1;
+    return 0;
 }
